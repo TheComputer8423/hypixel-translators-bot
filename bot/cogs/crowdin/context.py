e2fbf946e86c79605de9f98ba3e96fff3355f4dd
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-02-03 12:03:44 -0500

    Add context start

---
+++ bot/cogs/crowdin/context.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from main import main_db
+
+collection = main_db['context']
+
+
+class crowdin_context(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command()
+    async def rip_you_lmao(self, ctx):
+        await ctx.send("Have fun :kek:")
+
+
+def setup(bot):
+    bot.add_cog(crowdin_context(bot))
