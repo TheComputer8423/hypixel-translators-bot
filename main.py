670973ef4d9798f5a423e0c19386eb6f15a381f7
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-02-03 11:41:52 -0500

    Add database connection

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from discord import ActivityType, AllowedMentions
 from bot.Logger import Logger
 from discord.ext import commands
+from config import database_password
 import logging
 import discord
 import config
+import pymongo
 
-
+cluster = pymongo.MongoClient(f"mongodb+srv://rewrite:{database_password}@hypixeltranslators.gqhji.mongodb.net/htbmain\
+?retryWrites=true&w=majority")
+main_db = cluster['htbmain']
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(levelname)s - %(name)s] %(message)s")
 logging.getLogger("discord").setLevel(logging.WARNING)
