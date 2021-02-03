4308f4cfdbc02522c8653ef752893d9d7138bc09
marzeq <marzeqmarzeq@gmail.com>
2021-02-03 20:20:08 +0100

    Finished string utils, when rodry ports the users everything is ready to be used.

--- bot/utils/localization/localization.py
+++ bot/utils/localization/localization.py
@@ -1,10 +1,13 @@
 from typing import *
 import json
+from main import main_db
 
 
 async def get_strings(ctx) -> Tuple[dict, dict]:
-    author_lang = "en"  # todo: change to get from database
-    with open(f"strings/{author_lang}/{repr(ctx.command)}", "r") as f:
+    collection = main_db['test_players']  # todo: replace with players when rodry ports everything to the db
+    author_lang = collection.find_one({"id": ctx.author.id})["lang"]
+
+    with open(f"strings/{author_lang}/{ctx.command.name}.json", "r") as f:
         localstings = json.load(f)
     with open(f"strings/{author_lang}/global.json", "r") as f:
         globalstrings = json.load(f)
