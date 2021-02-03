446706608a988d6ab7c206c4444a44c10a17a7eb
HoggyTheWizard <57421011+HoggyTheWizard@users.noreply.github.com>
2021-02-03 11:47:00 -0500

    Add database_user

--- __config.py
+++ __config.py
@@ -16,6 +16,8 @@ owner_ids = [  # list of intigers of the ids of owners
 
 token = ""  # bot token
 
+database_user = "" # Database user
+
 database_password = "" # Password for the database 
 
 hypixel_api_key = ""  # hypixel server api key (hypixel.net on minecraft -> /api new)
