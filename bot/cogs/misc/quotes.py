69a75752218f75983954223701de0c26ecaa58fa
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-02-09 10:26:02 -0500

    Update quote add

--- bot/cogs/misc/quotes.py
+++ bot/cogs/misc/quotes.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from main import main_db
 from random import randint
-from config import mod_id, admin_id, staff_bot_channel_id
+from config import staff_bot_channel_id
 collection = main_db['quotes_test']
 
 
@@ -33,23 +33,34 @@ class quote(commands.Cog):
                 await ctx.send(embed=embed)
 
     @quote.command()
-    async def add(self, ctx, quote_author, *, quote_content: str):
-        mod_role = ctx.guild.get_role(mod_id)
-        admin_role = ctx.guild.get_role(admin_id)
-        quote_mods = [mod_role, admin_role]
+    async def add(self, ctx, quote_author: discord.Member, *, quote_content: str):
         target_channel = ctx.bot.get_channel(staff_bot_channel_id)
-        if quote_mods in ctx.author.roles:
-            # need to insert it into the database/make success message
-            print("Add stuff here later hoggy")
+        if ctx.message.author.guild_permissions.view_audit_log:
+            quote_stats = collection.find_one({"id": "eugene"})
+            total_quotes = quote_stats['total_quotes']
+            new_total_quotes = total_quotes + 1
+            author_id = quote_author.id
+            new_quote = {"id": new_total_quotes, "quote": quote_content, "author": f"<@!{author_id}>"}
+            collection.insert_one(new_quote)
+            collection.update_one({"id": "eugene"}, {"$set": {"total_quotes": new_total_quotes}})
+
+            staff_embed = discord.Embed(title="Success! The following quote was added:", color=discord.Colour.from_rgb(33, 222, 112))
+            staff_embed.set_author(name='Quote')
+            staff_embed.add_field(value=quote_content, name="Quote:", inline=False)
+            staff_embed.add_field(value=f'- <@!{author_id}>', name="Said By:", inline=False)
+            staff_embed.add_field(value=new_total_quotes, name="Quote ID:", inline=False)
+            staff_embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Executed by {ctx.author.name}')
+            await ctx.send(embed=staff_embed)
         else:
             user_embed = discord.Embed(title="Quote", color=discord.Colour.from_rgb(33, 222, 112))
             user_embed.add_field(value=f'\n{quote_content}\n- {quote_author}', name='Your quote request has been submitted, thanks!')
             user_embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Executed by {ctx.author.name}')
             await ctx.send(embed=user_embed)
 
-            # Change embed to actually work
-            target_embed = discord.Embed(title="Quote", color=discord.Colour.from_rgb(33, 222, 112))
-            target_embed.add_field(value=f'- {quote_author}', name='hi')
+            target_embed = discord.Embed(title="A quote request has been submitted!", color=discord.Colour.from_rgb(33, 222, 112))
+            target_embed.set_author(name='Quote')
+            target_embed.add_field(value=f"{quote_content}\n- <@!{quote_author.id}>", name='Quote:')
+            target_embed.add_field(value=f"`+quote add {quote_author.id} {quote_content}`", name='To add it', inline=False)
             target_embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Executed by {ctx.author.name}')
             await target_channel.send(embed=target_embed)
 
