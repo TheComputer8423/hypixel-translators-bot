8432151874b8ce7785878c92cf408d917412f7c3
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-02-10 19:33:26 -0500

    Add quote delete

--- bot/cogs/misc/quotes.py
+++ bot/cogs/misc/quotes.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from main import main_db
 from random import randint
 from config import staff_bot_channel_id
+# remove "_test" on the main instance
 collection = main_db['quotes_test']
 
 
@@ -64,6 +65,45 @@ class quote(commands.Cog):
             target_embed.add_field(value=f"`+quote add {quote_author.id} {quote_content}`", name='To add it', inline=False)
             target_embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Executed by {ctx.author.name}')
             await target_channel.send(embed=target_embed)
+
+    @quote.command()
+    async def delete(self, ctx, quote_id: int = None):
+        if ctx.message.author.guild_permissions.view_audit_log:
+            if quote_id is None:
+                embed = discord.Embed(title='Error',
+                                      description='You did not provide a quote ID!', color=discord.Colour.red())
+                embed.add_field(value='`+quote delete <quote_id>`', name='Usage:', inline=False)
+                embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Executed by {ctx.author.name}')
+                await ctx.send(embed=embed)
+            else:
+                quote_data = collection.find_one({"id": quote_id})
+                if quote_data is None:
+                    embed = discord.Embed(title='Error',
+                                          description="I couldn't find a quote with that ID!", color=discord.Colour.red())
+                    embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Executed by {ctx.author.name}')
+                    await ctx.send(embed=embed)
+                else:
+                    collection.delete_one(quote_data)
+                    quote_stats = collection.find_one({"id": "eugene"})
+                    total_quotes = quote_stats['total_quotes']
+                    new_total_quotes = total_quotes - 1
+                    collection.update_one({"id": "eugene"}, {"$set": {"total_quotes": new_total_quotes}})
+                    quote_author = quote_data['author']
+
+                    staff_embed = discord.Embed(title=f"Successfully deleted quote #{quote_id}", color=discord.Colour.
+                                                from_rgb(33, 222, 112))
+                    staff_embed.set_author(name='Quote')
+                    staff_embed.add_field(value=quote_data['quote'], name="Quote:", inline=False)
+                    staff_embed.add_field(value=f'- {quote_author}', name="Said By:", inline=False)
+                    staff_embed.add_field(value=quote_data['id'], name="Quote ID:", inline=False)
+                    staff_embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Executed by {ctx.author.name}')
+                    await ctx.send(embed=staff_embed)
+        else:
+            embed = discord.Embed(title='Error',
+                                  description="You don't have permission to use this command!",
+                                  color=discord.Colour.red())
+            embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Executed by {ctx.author.name}')
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
